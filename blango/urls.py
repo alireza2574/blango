94afaec20885d14c19755b110f07a1981505517c
alireza sanati <alirezahosseinpour940@gmail.com>
2024-07-15 18:02:10 +0000

    Finish django Allauth

--- blango/urls.py
+++ blango/urls.py
@@ -35,6 +35,8 @@ urlpatterns = [
     name="django_registration_register",
 ),
     path("accounts/", include("django_registration.backends.activation.urls")),
+    path("accounts/", include("allauth.urls")),
+    
 ]
 
 if settings.DEBUG:
