2df2c9b33984b7fc10cc3aba6d8f0fef05d65685
alireza sanati <alirezahosseinpour940@gmail.com>
2024-07-15 16:33:02 +0000

    Finish custom User model

--- blango/settings.py
+++ blango/settings.py
@@ -84,6 +84,7 @@ class Dev(Configuration):
         'crispy_forms',
         'crispy_bootstrap5',
         'debug_toolbar',
+        'blango_auth',
     ]
 
     MIDDLEWARE = [
@@ -172,7 +173,7 @@ class Dev(Configuration):
     # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
     DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+    AUTH_USER_MODEL = "blango_auth.User"
     CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
     CRISPY_TEMPLATE_PACK = "bootstrap5"
 
